509b0816554c7efb4fab46b89fbf3a71b8df9cc1
mendoza-git <mendoza.getmail@gmail.com>
2018-07-18 10:39:12 -0500

    tabs added to share SPA content

--- src/app/graphs/graphs.component.ts
+++ src/app/graphs/graphs.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-graphs',
-  template: `<h1>it works</h1>`,
   templateUrl: './graphs.component.html',
   styleUrls: ['./graphs.component.css']
 })
