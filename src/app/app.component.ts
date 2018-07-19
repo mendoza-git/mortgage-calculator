58ecd6f45889be5dbf5579e86f5f94007d8e0d3b
mendoza-git <mendoza.getmail@gmail.com>
2018-07-19 08:58:30 -0500

    demo ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,14 +10,19 @@ export class AppComponent {
   loanAmount: number = 150000;
   interestRate: number = 4.25;
   numYears: number = 30;
+
+  paymentArray = [];
+  interestArray = [];
+  principalArray = [];
+  remainingBalanceArray = [];
+
   n: number;
   rate: number;
   total_months: number;
-    
   total_principal: number;
   monthly_payment: number;
 
-  constructor() { }
+  constructor() {}
 
   num_payments(){
     this.n = this.numYears * 12;
@@ -36,10 +41,35 @@ export class AppComponent {
   calculate_monthly_payments() {
     this.monthly_payment = (Math.round(( this.loanAmount * ( this.rate * Math.pow((1 + this.rate), this.n))) / (Math.pow((1+this.rate), this.n) - 1)));
     return this.monthly_payment;
+  }
+
+  generateAmorizationArray() {
+
+    var total_payments = this.numYears * 12;
+    var current_payment = 0;
+
+    var current_balance = this.loanAmount;
+    var interestRate = this.interestRate / 1200;
+
+    while (current_payment <= total_payments) {
+      var n = current_balance * interestRate;
+      var m = current_payment - n;
+
+      this.paymentArray.push(this.loanAmount * (this.interestRate / 1200));
+      this.interestArray.push(n);
+      this.principalArray.push(m);
+
+      current_balance = current_balance - n;
+      this.remainingBalanceArray.push(current_balance);
+
+      current_payment++;
+    }
 
   }
 
   ngOnInit() {
+
   }
+   
 }
 
